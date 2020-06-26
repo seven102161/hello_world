0e4d80b50bd84c9b04b27abfb9361760ae285bce
Seven <52610264+seven102161@users.noreply.github.com>
2020-06-26 14:42:41 +0800

    add some notes

--- an.business_data_analysis.py
+++ an.business_data_analysis.py
@@ -26,18 +26,44 @@ memory usage: 1.5+ KB
 print('-------------------------------------\n')
 mean_area = (df.loc[:, ['area', 'daily_price', 'monthly_price_size']].groupby('area').mean())
 mean_area = mean_area.reset_index()
-print('按楼层区域划分: \n', mean_area)
+print('按楼层区域划分: 【日单价（元/平方） 月单价（元/平方）】\n', mean_area)
 print('-------------------------------------\n')
+
+'''
+按楼层区域划分【日单价（元/平方） 月单价（元/平方）】: 
+   area  daily_price  monthly_price_size
+0   中区     1.864286           55.910718
+1   高区     1.790000           53.691892
+'''
 
 mean_floors = (df.loc[:, ['total_floors', 'daily_price', 'monthly_price_size']].groupby('total_floors').mean())
 mean_floors = mean_floors.reset_index()
-print('按最高楼层划分: \n', mean_floors)
+print('按最高楼层划分: 【日单价（元/平方） 月单价（元/平方）】\n', mean_floors)
 print('-------------------------------------\n')
+
+'''
+按最高楼层划分: 【日单价（元/平方） 月单价（元/平方）】
+   total_floors  daily_price  monthly_price_size
+0          20层     1.860000           55.774123
+1          24层     2.050000           61.500000
+2          25层     1.872222           56.148766
+3          26层     1.400000           42.000000
+4          31层     1.700000           51.000000
+'''
 
 mean_size = (df.loc[:, ['size_range', 'daily_price', 'monthly_price_size']].groupby('size_range').mean())
 mean_size = mean_size.reset_index()
-print('按楼出租面积划分: \n', mean_size)
+print('按楼出租面积划分:【日单价（元/平方） 月单价（元/平方）】 \n', mean_size)
 print('-------------------------------------\n')
+
+'''
+按楼出租面积划分: 【日单价（元/平方） 月单价（元/平方）】
+      size_range  daily_price  monthly_price_size
+0      (0, 100]     1.400000           42.000000
+1    (100, 500]     1.866667           55.983076
+2  (1000, 2000]     1.940000           58.194721
+3   (500, 1000]     1.787500           53.597054
+'''
 
 
 p1 = plt.figure(figsize=(12, 6))
@@ -56,5 +82,5 @@ y = df['monthly_price_size']
 plt.bar(x, y, color='sandybrown')
 plt.xticks(x)
 
-plt.savefig('an_business.png')
-plt.show()
+# plt.savefig('an_business.png')
+# plt.show()
